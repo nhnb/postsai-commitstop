d588e5fcc4adb8f28d3efd1cbb9b1b497cb61e4f
Hendrik Brummermann <brummermann@his.de>
2026-08-17 19:20:47 +0200

    updated build script and copyright year

--- dist.sh
+++ dist.sh
@@ -6,22 +6,18 @@ TMPDIR=`mktemp -d`
 # Clean build of angular app
 cd frontend
 rm -rf dist
-ng build --prod --aot
+ng build
 cd ..
 cp -ax . $TMPDIR/commitstop
 
 # Delete unnecessary files
-mv $TMPDIR/commitstop/frontend/dist $TMPDIR/
+mv $TMPDIR/commitstop/frontend/dist/frontend/browser $TMPDIR/
 rm -rf $TMPDIR/commitstop/.git $TMPDIR/commitstop/dist.sh $TMPDIR/commitstop/frontend
 rm `find $TMPDIR -name *.pyc`
-mv $TMPDIR/dist $TMPDIR/commitstop/frontend
+mv $TMPDIR/browser $TMPDIR/commitstop/frontend
 
 # adjust relativ path because path is just "frontend" in distribution instead of "frontend/dist" 
-export JS_FILE=`ls $TMPDIR/commitstop/frontend/main-es2015.*`
-sed "s|\.\./\.\./api.py|../api.py|" < $JS_FILE > $TMPDIR/t.txt
-mv $TMPDIR/t.txt $JS_FILE
-
-export JS_FILE=`ls $TMPDIR/commitstop/frontend/main-es5.*`
+export JS_FILE=`ls $TMPDIR/commitstop/frontend/main*.js`
 sed "s|\.\./\.\./api.py|../api.py|" < $JS_FILE > $TMPDIR/t.txt
 mv $TMPDIR/t.txt $JS_FILE
 
